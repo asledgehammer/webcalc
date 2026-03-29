37cac05c7993b6097a8b38b56c7d2d9229293ef4
Jab <JabDoesThings@protonmail.com>
2026-03-29 14:46:50 -0500

    Removed tools/ folder

--- tools/mvn_install_pz.sh
+++
@@ -1,6 +0,0 @@
-# Use ENV File Binaries
-source ../.env
-
-# Build local Artifact for Project Zomboid
-mvn install:install-file -Dfile="${PZ_HOME}/${MVN_TARGET}" -DgroupId="${MVN_GROUP}" -DartifactId="${MVN_ARTIFACT}" -Dversion="${PZ_VERSION}" -Dpackaging=jar
-# mvn
